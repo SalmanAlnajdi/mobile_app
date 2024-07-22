64d3950cfadde7c37e499e575adc6a9cb16809ca
q8wahab <q8_xprt@hotmail.com>
2024-07-22 04:09:27 +0300

    add the login page and test and register page and test all ok

--- src/screens/landingPages/Login.js
+++ src/screens/landingPages/Login.js
@@ -1,18 +1,73 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Button, Text, TextInput, View, Alert } from "react-native";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { login } from "../../apis/auth";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigation = useNavigation();
+
+  const handleLogin = async () => {
+    try {
+      const data = await login({ username, password });
+      // Handle successful login
+      console.log(data);
+      Alert.alert("Success", "You have logged in successfully");
+    } catch (error) {
+      // Handle login error
+      console.error(error);
+      Alert.alert("Error", "Invalid username or password");
+    }
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 30 }}>Login Screen</Text>
-      <Button
-        title="Register"
-        onPress={() => {
-          navigation.navigate("Register");
+      <Text style={{ fontSize: 30, marginBottom: 20 }}>Login Screen</Text>
+      <TextInput
+        style={{
+          width: "80%",
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 12,
+          paddingLeft: 8,
         }}
-      ></Button>
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={{
+          width: "80%",
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 12,
+          paddingLeft: 8,
+        }}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+      <View style={{ marginTop: 10 }}>
+        <Button
+          title="Register"
+          onPress={() => {
+            navigation.navigate("Register");
+          }}
+        />
+      </View>
+      <View style={{ marginTop: 10 }}>
+        <Button
+          title="Forgot Password"
+          onPress={() => {
+            navigation.navigate("ForgotPassword");
+          }}
+        />
+      </View>
     </View>
   );
 };
