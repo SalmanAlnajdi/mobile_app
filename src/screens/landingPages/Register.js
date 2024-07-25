bf6172658ed5fa90f93fa71e5257103d7a6f93d4
q8wahab <q8_xprt@hotmail.com>
2024-07-25 23:59:22 +0300

    login and signup design

--- src/screens/landingPages/Register.js
+++ src/screens/landingPages/Register.js
@@ -1,8 +1,19 @@
-import { Button, Text, TextInput, View, Alert, ScrollView } from "react-native";
+import {
+  Button,
+  Text,
+  TextInput,
+  View,
+  Alert,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import React, { useContext, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { register } from "../../apis/auth"; // Ensure this path is correct
 import UserContext from "../../context/UserContext";
+import { LinearGradient } from "expo-linear-gradient";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { RadioButton } from "react-native-paper";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -49,126 +60,166 @@ const Register = () => {
         flexGrow: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#1E1E2B",
       }}
     >
-      <Text style={{ fontSize: 30, marginBottom: 20 }}>Register Screen</Text>
+      <Text style={{ fontSize: 15, marginBottom: 20, color: "#fff" }}>
+        Create an account{" "}
+      </Text>
+
       <TextInput
-        style={{
-          width: "80%",
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12,
-          paddingLeft: 8,
-        }}
+        style={styles.input}
         placeholder="First Name"
+        placeholderTextColor="#aaa"
         value={firstName}
         onChangeText={setFirstName}
       />
       <TextInput
-        style={{
-          width: "80%",
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12,
-          paddingLeft: 8,
-        }}
+        style={styles.input}
         placeholder="Last Name"
+        placeholderTextColor="#aaa"
         value={lastName}
         onChangeText={setLastName}
       />
       <TextInput
-        style={{
-          width: "80%",
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12,
-          paddingLeft: 8,
-        }}
+        style={styles.input}
         placeholder="Username"
+        placeholderTextColor="#aaa"
         value={username}
         onChangeText={setUsername}
       />
       <TextInput
-        style={{
-          width: "80%",
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12,
-          paddingLeft: 8,
-        }}
+        style={styles.input}
         placeholder="Password"
+        placeholderTextColor="#aaa"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
       <TextInput
-        style={{
-          width: "80%",
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12,
-          paddingLeft: 8,
-        }}
+        style={styles.input}
         placeholder="Confirm Password"
+        placeholderTextColor="#aaa"
         value={confirmPassword}
         onChangeText={setConfirmPassword}
         secureTextEntry
       />
       <TextInput
-        style={{
-          width: "80%",
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12,
-          paddingLeft: 8,
-        }}
+        style={styles.input}
         placeholder="Phone"
+        placeholderTextColor="#aaa"
         value={phone}
         onChangeText={setPhone}
       />
-      <TextInput
-        style={{
-          width: "80%",
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12,
-          paddingLeft: 8,
-        }}
+      {/* <TextInput
+        style={styles.input}
         placeholder="Gender"
+        placeholderTextColor="#aaa"
         value={gender}
         onChangeText={setGender}
-      />
+      /> */}
+      <View style={styles.radioContainer}>
+        <Text style={styles.radioText}>Gender:</Text>
+
+        <RadioButton.Group
+          onValueChange={(newValue) => setGender(newValue)}
+          value={gender}
+        >
+          <View style={styles.radioRow}>
+            <View style={styles.radioButton}>
+              <RadioButton value="male" />
+              <Text style={styles.radioLabel}>Male</Text>
+            </View>
+            <View style={styles.radioButton}>
+              <RadioButton value="female" />
+              <Text style={styles.radioLabel}>Female</Text>
+            </View>
+          </View>
+        </RadioButton.Group>
+      </View>
       <TextInput
-        style={{
-          width: "80%",
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12,
-          paddingLeft: 8,
-        }}
+        style={styles.input}
         placeholder="Email"
+        placeholderTextColor="#aaa"
         value={email}
         onChangeText={setEmail}
       />
-      <Button title="Register" onPress={handleRegister} />
-      <View style={{ marginTop: 10 }}>
-        <Button
-          title="Back to Login"
+      <LinearGradient
+        colors={["#4D81D3", "#9765B5"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={{
+          padding: 15,
+          borderRadius: 10,
+          marginBottom: 20,
+          width: "80%",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity onPress={handleRegister}>
+          <Text style={styles.signInButtonText}>Sign up</Text>
+        </TouchableOpacity>
+      </LinearGradient>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity
           onPress={() => {
             navigation.navigate("Login");
           }}
-        />
+        >
+          <Text>
+            <Text style={styles.signupText}>Already have an account?</Text>
+            <Text style={styles.signinText}> Signin</Text>
+          </Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
 };
+const styles = StyleSheet.create({
+  input: {
+    width: "80%",
+    height: 40,
+    borderColor: "#4583D5",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingLeft: 8,
 
+    placeholderTextColor: "FFFFFF",
+    backgroundColor: "#1E1E2B",
+    color: "#fff",
+    padding: 10,
+    borderRadius: 10,
+  },
+  signupText: {
+    color: "#fff",
+  },
+  signInButtonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  signinText: {
+    color: "#4a90e2",
+  },
+  radioContainer: {
+    width: "80%",
+    marginBottom: 12,
+  },
+  radioText: {
+    color: "#fff",
+    marginBottom: 8,
+  },
+  radioButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: "#fff",
+  },
+  radioLabel: {
+    color: "#fff",
+    marginLeft: 8,
+  },
+  radioRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
 export default Register;
