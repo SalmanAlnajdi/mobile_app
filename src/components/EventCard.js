cd3c622df25726afd4ff2da450957bbca5aac865
q8wahab <q8_xprt@hotmail.com>
2024-07-26 21:42:06 +0300

    home design finish

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,65 +1,90 @@
-import { Button, Image, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import getAllEvents from "../apis/events";
 import { useQuery } from "@tanstack/react-query";
 import getUserById from "../apis/users";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const EventCard = ({ event }) => {
   const navigation = useNavigation();
-  // const {
-  //   data: user,
-  //   isLoading,
-  //   error,
-  // } = useQuery({
-  //   queryKey: ["user", event.owner],
-  //   queryFn: () => getUserById(event.owner),
-  // });
-
-  // console.log("User Data:", user);
-  // console.log("Error:", error);
-
-  // if (isLoading) {
-  //   return <Text>Loading...</Text>;
-  // }
-
-  // if (error) {
-  //   return <Text>Error loading user details</Text>;
-  // }
 
   return (
-    <View
-      style={{
-        height: 250,
-        width: 300,
-        justifyContent: "center",
-        alignItems: "center",
-        border: "1px solid black",
-
-        padding: 10,
-        borderRadius: 24,
-        backgroundColor: "gray",
-
-        margin: 10,
-      }}
-    >
-      <Text>Created by: {event?.owner?.username}</Text>
-      {/* <Text>Created by2: {user?.username}</Text> */}
-      <Text>Event Card</Text>
-      <Text>{event.name}</Text>
-      <Text>{event.description}</Text>
-      <Text>{event.date}</Text>
-      <Text>{event.time}</Text>
-      <Text>{event.location}</Text>
-      <Text>{event.image}</Text>
-      <Button
-        title="View Details"
-        onPress={() => {
-          navigation.navigate("EventDetail", { event });
-        }}
-      />
+    <View style={styles.card}>
+      <Image source={{ uri: event.image }} style={styles.image} />
+      <View style={styles.content}>
+        {/* <Text style={styles.status}>Live</Text> */}
+        <Text style={styles.title}>{event.name}</Text>
+        <Text style={styles.location}>{event.location}</Text>
+        <Text style={styles.description}>{event.description}</Text>
+        <Text style={styles.owner}>Created by: {event?.owner?.username}</Text>
+        <Text style={styles.date}>Event Date: {event.date}</Text>
+        <Text style={styles.time}>Event Time: {event.time}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("EventDetail", { event });
+          }}
+        >
+          <Text style={styles.seeMore}>...see more</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#323048",
+    borderRadius: 10,
+    overflow: "hidden",
+    marginBottom: 20,
+  },
+  image: {
+    width: 350,
+    height: 150,
+  },
+  content: {
+    padding: 15,
+  },
+  status: {
+    color: "#a0a0e0",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 5,
+  },
+  location: {
+    color: "#ccc",
+    fontSize: 16,
+    marginVertical: 5,
+  },
+  description: {
+    color: "#ddd",
+    fontSize: 14,
+    marginVertical: 10,
+  },
+  owner: {
+    color: "#bbb",
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  date: {
+    color: "#bbb",
+    fontSize: 14,
+  },
+  time: {
+    color: "#bbb",
+    fontSize: 14,
+    marginBottom: 10,
+  },
+  seeMore: {
+    color: "#a0a0e0",
+    fontSize: 14,
+    textAlign: "right",
+  },
+});
 export default EventCard;
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,62 +1,95 @@
-import { ScrollView, Text, View } from "react-native";
-import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
 import EventCard from "../../components/EventCard";
 import { useQuery } from "@tanstack/react-query";
 import getAllEvents from "../../apis/events";
 
 const HomeScreen = () => {
-  const { data: Event } = useQuery({
+  const { data: events } = useQuery({
     queryKey: ["getAllEvents"],
     queryFn: () => getAllEvents(),
   });
 
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (text) => {
+    setSearchQuery(text);
+    // You can add search filtering logic here if needed
+  };
+
   return (
-    <View
-      style={{
-        width: "100%",
-        alignItems: "center",
-        backgroundColor: "#1E1E2B",
-      }}
-    >
-      <View
-        style={{
-          width: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20,
-            color: "white",
-            borderColor: "red",
-            borderWidth: 1,
-            borderRadius: 10,
-            width: "90%",
-            textAlign: "center",
-            padding: 10,
-          }}
-        >
-          Search Events
-        </Text>
-        <ScrollView
-          style={{
-            height: "100%",
-            width: "90%",
-
-            flexWrap: "wrap",
-            flexDirection: "column",
-
-            padding: 20,
-          }}
-        >
-          {Event?.map((event) => (
-            <EventCard event={event} key={event._id} />
-          ))}
-        </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search..."
+          placeholderTextColor="#888"
+          value={searchQuery}
+          onChangeText={handleSearchChange}
+        />
+        <TouchableOpacity style={styles.filterButton}>
+          <Image
+            // source={require("../../assets/filter-icon.png")} // Add your filter icon image here
+            style={styles.filterIcon}
+          />
+        </TouchableOpacity>
       </View>
+      <ScrollView style={styles.scrollView}>
+        {events?.map((event) => (
+          <EventCard event={event} key={event._id} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "#1E1E2B",
+    padding: 20,
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 20,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    backgroundColor: "#29293E",
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    color: "#FFF",
+    fontSize: 16,
+    marginRight: 10,
+  },
+  filterButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+    backgroundColor: "#6165F6",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  filterIcon: {
+    width: 20,
+    height: 20,
+    tintColor: "#FFF",
+  },
+  scrollView: {
+    width: "100%",
+  },
+});
 
 export default HomeScreen;
