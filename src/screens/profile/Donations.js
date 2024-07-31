3446476180639b7267322c27e3a8a601a5f48c43
SalmanAlnajdi <salman.it095@gmail.com>
2024-07-31 13:42:52 +0300

    Order Card And Donatios Page Done

--- src/screens/profile/Donations.js
+++ src/screens/profile/Donations.js
@@ -1,7 +1,18 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import getAllOrders from "../../apis/order";
+import { useNavigation } from "@react-navigation/native";
+import OrderCard from "../../components/OrderCard";
 
 const Donations = () => {
+  const navigation = useNavigation();
+
+  const { data: orders } = useQuery({
+    queryKey: ["getAllOrders"],
+    queryFn: () => getAllOrders(),
+  });
+
   return (
     <View
       style={{
@@ -11,7 +22,23 @@ const Donations = () => {
         backgroundColor: "#1E1E2B",
       }}
     >
-      <Text>Donations</Text>
+      <ScrollView>
+        {orders?.map((order) => (
+          <OrderCard key={order._id} order={order} />
+        ))}
+        {/* {orders?.map((order) => (
+        <View key={order._id}>
+        <Text>{order._id}</Text>
+        {order.items.map((item) => (
+            <View key={item._id}>
+            <Text>{item.image}</Text>
+            <Text>{item.name}</Text>
+              <Text>{item.description}</Text>
+              </View>
+              ))}
+              </View>
+              ))} */}
+      </ScrollView>
     </View>
   );
 };
